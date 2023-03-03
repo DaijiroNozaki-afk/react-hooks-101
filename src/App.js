fbb9d822096773b8a4189dfdd916f4d3a8478602
dnozaki <dnozaki88888888@gmail.com>
2023-03-03 12:28:04 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount ] = useState(0)
+const App = props => {
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-
-  const reset = () => setCount(0)
-  const multiple2 = () => setCount(count * 2)
-  const divisionIf3 = () => setCount((count%3 === 0) ? count / 3:count)
-
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+ 
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <React.Fragment>
-    <div>count: {count}</div>
-    <div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-    </div>
-    <div>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-    </div>
-    <div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
       <button onClick={reset}>Reset</button>
-      <button onClick={multiple2}>x2</button>
-      <button onClick={divisionIf3}>3の倍数の時だけ3で割る</button>
-    </div>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </React.Fragment>
   );
 }
-
+App.defaultProps = {
+  name: '',
+  price: 1000
+}
 export default App;
